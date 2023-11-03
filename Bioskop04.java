a9cf6b0317ee5501c69c38cc3cb40564cc8e5404
ArjnCrly <144399335+ArjnCrly@users.noreply.github.com>
2023-11-03 08:36:11 +0700

    mod

--- Bioskop04.java
+++ Bioskop04.java
@@ -20,13 +20,11 @@ public class Bioskop04 {
         System.out.println(penonton.length);
 
         for (String[] barisPenonton : penonton){ 
-            System.out.println("Panjang baris : " + barisPenonton.length); {
-        
-            System.out.println("Penonton pada baris ke-3 : ");
+            System.out.println("Panjang baris : " + barisPenonton.length); 
              
-            for (String i : penonton[2]){
-                System.out.println(i);
-            }   
+            for (int i = 0; i < penonton.length; i++){
+                System.out.println("Penonton pada baris ke-" + (i+1) + ": "  + String.join(", ", penonton[i]));
+            
             }
         }
     }
